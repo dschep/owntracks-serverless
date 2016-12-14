5a19b536ed4cbbd5a0df774f6bbc15368ab860a9
Daniel Schep <dschep@gmail.com>
2016-12-14 00:51:26 -0500

    save user and device info!

--- handler.js
+++ handler.js
@@ -10,6 +10,12 @@ module.exports.pub = (event, context, callback) => {
   const item = _.pick(JSON.parse(event.body),
       ['acc', 'alt', 'batt', 'cog', 'desc', 'event', 'lat', 'lon', 'rad', 't',
       'tid', 'tst', 'vac', 'vel', 'p', 'conn']);
+  if (event.queryStringParameters)
+    Object.assign(item, _.pick(event.queryStringParameters, ['u', 'd']))
+  if (event.headers['X-Limit-U'])
+    item.u = event.headers['X-Limit-U']
+  if (event.headers['X-Limit-D'])
+    item.d = event.headers['X-Limit-D']
 
   docClient.putAsync({
     TableName: process.env.table,
